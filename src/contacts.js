4cfecc6c48cfbc17bce137b2531de6744677cbd1
Nasir Ahmed <nasir.amath07@gmail.com>
2025-06-17 10:18:50 +0600

    fix: remove localforage dependency

--- src/contacts.js
+++ src/contacts.js
@@ -4,8 +4,8 @@ import sortBy from "sort-by";
 
 export async function getContacts(query) {
   await fakeNetwork(`getContacts:${query}`);
-  let contacts = await localforage.getItem("contacts");
-  if (!contacts) contacts = [];
+  const response = await fetch("http://localhost:3000/contacts");
+  let contacts = await response.json();
   if (query) {
     contacts = matchSorter(contacts, query, { keys: ["first", "last"] });
   }
@@ -15,39 +15,48 @@ export async function getContacts(query) {
 export async function createContact() {
   await fakeNetwork();
   let id = Math.random().toString(36).substring(2, 9);
-  let contact = { id, createdAt: Date.now() };
-  let contacts = await getContacts();
-  contacts.unshift(contact);
-  await set(contacts);
+  let payload = { id, createdAt: Date.now() };
+  const response = await fetch("http://localhost:3000/contacts", {
+    method: "POST",
+    body: JSON.stringify(payload),
+  });
+  const contact = response.json();
   return contact;
 }
 
 export async function getContact(id) {
   await fakeNetwork(`contact:${id}`);
-  let contacts = await localforage.getItem("contacts");
-  let contact = contacts.find((contact) => contact.id === id);
+  const response = await fetch(`http://localhost:3000/contacts/${id}`);
+  let contact = await response.json();
   return contact ?? null;
 }
 
 export async function updateContact(id, updates) {
   await fakeNetwork();
-  let contacts = await localforage.getItem("contacts");
-  let contact = contacts.find((contact) => contact.id === id);
-  if (!contact) throw new Error("No contact found for", id);
+  const contact = await getContact(id);
   Object.assign(contact, updates);
-  await set(contacts);
-  return contact;
+  const response = await fetch(`http://localhost:3000/contacts/${id}`, {
+    method: "PUT",
+    body: JSON.stringify(contact),
+  });
+  const nextContact = await response.json();
+  return nextContact;
 }
 
 export async function deleteContact(id) {
-  let contacts = await localforage.getItem("contacts");
-  let index = contacts.findIndex((contact) => contact.id === id);
-  if (index > -1) {
-    contacts.splice(index, 1);
-    await set(contacts);
-    return true;
-  }
-  return false;
+  const response = await fetch(`http://localhost:3000/contacts/${id}`, {
+    method: "DELETE",
+  });
+  let contact = await response.json();
+  return contact ? true : false;
+
+  // let index = contacts.findIndex((contact) => contact.id === id);
+  // if (index > -1) {
+  //   contacts.splice(index, 1);
+  //   await set(contacts);
+  //   return true;
+  // }
+  // return false;
 }
 
 function set(contacts) {
